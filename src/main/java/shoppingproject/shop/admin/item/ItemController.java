24c08a52867bfce6f47bf266b7cbd9c094e021c9
sallypark185 <56714558+sallypark185@users.noreply.github.com>
2023-07-12 13:50:02 +0900

    Item Management CRUD 2023-07-12

---
+++ src/main/java/shoppingproject/shop/admin/item/ItemController.java
@@ -0,0 +1,74 @@
+package shoppingproject.shop.admin.item;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import shoppingproject.shop.model.item.Item;
+import shoppingproject.shop.model.item.ItemRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Slf4j
+@Controller
+@RequestMapping("/admin/item")
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemRepository itemRepository;
+    //목록 폼
+    @GetMapping("/list")
+    public String list(Model model ){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "/admin/item/list";
+    }
+
+    //등록 폼
+    @GetMapping("/add")
+    public String addFrom(){
+        log.info("item.add");
+
+        return "/admin/item/add";
+    }
+
+    //등록 로직
+    @PostMapping("/add")
+    public String add(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+        return "redirect:/admin/item/list";
+    }
+
+    //상세 폼
+    @GetMapping("/detail/{id}")
+    public String detailForm(Model model, @PathVariable Long id){
+        log.info("item.detail");
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item",item);
+        return "/admin/item/detail";
+    }
+
+    //수정 폼
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable Long id,Model model){
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item",item);
+        return "/admin/item/edit";
+    }
+
+    //상세 로직
+    @PostMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, @ModelAttribute Item item){
+        itemRepository.update(id,item);
+        return "redirect:/admin/item/list";
+    }
+
+    //삭제 로직
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        itemRepository.delete(id);
+        return "redirect:/admin/item/list";
+    }
+
+}
---
+++ src/main/java/shoppingproject/shop/model/item/Item.java
@@ -0,0 +1,15 @@
+package shoppingproject.shop.model.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+ private Long Id;
+ private String ItemName;
+ private Integer Price;
+ private Integer Quantity;
+ private String Description;
+
+}
